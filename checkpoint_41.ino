b1c115e9019ed9b70ef9dd8b8f9f9cd35fe649e2
Gabriel Torres <126830021+gtorresluiz@users.noreply.github.com>
2023-10-04 16:44:11 -0300

    Add files via upload

---
+++ checkpoint_41.ino
@@ -0,0 +1,143 @@
+// Checkpoint02
+
+#include<LiquidCrystal.h> // biblioteca do lcd
+
+LiquidCrystal lcd(7,6,5,4,3,2); // RS, E, D4, D5, D6, D7
+
+int ledVermelho = 11;
+int ledAmarelo = 12;
+int ledVerde = 13;
+int buzzer = 10;
+int sensorLDR = A5;
+int sensorTMP = A4;
+
+int limiteLDR = 600;
+
+double tensao = 0; //declarando as variaveis
+double temp = 0;
+
+void setup()
+{
+  pinMode(ledVermelho, OUTPUT);
+  pinMode(ledAmarelo, OUTPUT);
+  pinMode(ledVerde, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(sensorLDR, INPUT);
+  pinMode(sensorTMP, INPUT);
+  
+  Serial.begin(9600);
+  lcd.begin(16,2); 
+   
+}
+
+void loop(){
+  
+  int sensorUmidade = analogRead(A3);
+  int umidade = (map(sensorUmidade, 0, 1023, 10, 70));
+  int leituraL = analogRead(sensorLDR);
+  int leituraT = analogRead(sensorTMP);
+  tensao = (leituraT / 1023.0) * 5000; // calculo pra simular o sensor dht
+  temp = (tensao - 500) * 0.1;
+  
+  Serial.print("\t Iluminação: ");
+  Serial.print(leituraL, 1);
+  
+  Serial.print("\t Umidade: "); //valores de umidade no monitor
+  Serial.print(map(sensorUmidade, 0, 1023, 10, 70));
+  Serial.println("%");
+  
+  Serial.print("\t Temperatura = "); //valores de temperatura no monitor
+  Serial.print(temp,1);
+
+ if ((leituraL >= 90) && (leituraL <= 70)) {
+    lcd.clear();
+    digitalWrite(ledAmarelo, HIGH);
+    digitalWrite(ledVermelho, LOW);
+    digitalWrite(ledVerde, LOW);
+    digitalWrite(buzzer, LOW);
+    lcd.setCursor(0,0);
+    lcd.print("Ambiente a meia");
+    lcd.setCursor(0,1);
+    lcd.print("luz");
+    delay(5000);
+      
+  }
+  else {
+    lcd.clear();
+    digitalWrite(ledAmarelo, LOW);
+    digitalWrite(ledVerde, LOW);
+    digitalWrite(ledVermelho, HIGH);
+    digitalWrite(buzzer, HIGH); // liga o buzzer
+    lcd.setCursor(0,0);
+    lcd.print("Iluminação");
+    lcd.setCursor(0,1);
+    lcd.print("Irregular ");
+    delay(5000);
+  }    
+  if ((temp >= 8) && (temp <= 13)){
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Temperatura OK");
+      lcd.setCursor(0,1);
+	  lcd.print("Temp. = ");
+	  lcd.print(temp, 1);
+      lcd.print("C");
+      delay(5000);
+  }
+  else if (temp > 13){
+    lcd.clear();
+    digitalWrite(ledAmarelo, HIGH);
+    digitalWrite(buzzer, HIGH);
+    lcd.setCursor(0,0);
+    lcd.print("Temp. ALTA ");
+    lcd.setCursor(0,1);
+    lcd.print("Temp. = ");
+    lcd.print(temp, 1);
+    lcd.print("C");
+    delay(5000);
+  }
+  else{
+    lcd.clear();
+    digitalWrite(ledAmarelo, HIGH);
+    digitalWrite(buzzer, HIGH);
+    lcd.setCursor(0,0);
+    lcd.print("Temp. BAIXA ");
+    lcd.setCursor(0,1);
+    lcd.print("Temp. = ");
+    lcd.print(temp, 1);
+    lcd.print("C");
+    delay(5000);
+  }
+  
+  if ((umidade >= 60) && (umidade <= 80)){
+    digitalWrite(buzzer, LOW);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Umidade OK ");
+    lcd.setCursor(0,1);
+	lcd.print("Umidade = ");
+	lcd.print(umidade, 1);
+    lcd.print("%");
+    delay(5000);
+  }
+  else if (umidade <= 59){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Umidade BAIXA ");
+    lcd.setCursor(0,1);
+	lcd.print("Umidade = ");
+	lcd.print(umidade, 1);
+    lcd.print("%");
+    delay(5000);
+  }
+  else{
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Umidade ALTA ");
+    lcd.setCursor(0,1);
+	lcd.print("Umidade = ");
+	lcd.print(umidade, 1);
+    lcd.print("%");
+    delay(5000);
+  } 
+}
