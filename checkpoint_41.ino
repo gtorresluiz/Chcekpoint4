b10e6043f3b7a7bb178056475497334a964035cf
Gabriel Torres <126830021+gtorresluiz@users.noreply.github.com>
2023-10-04 21:10:39 -0300

    Update checkpoint_41.ino

--- checkpoint_41.ino
+++ checkpoint_41.ino
@@ -1,143 +1,118 @@
-// Checkpoint02
+#include "Arduino.h"
+#include "WiFi.h"
+#include <HTTPClient.h>
+#include "DHT.h"
 
-#include<LiquidCrystal.h> // biblioteca do lcd
+HTTPClient client;
+#define DHTTYPE DHT11
 
-LiquidCrystal lcd(7,6,5,4,3,2); // RS, E, D4, D5, D6, D7
+char nomeWifi[] = "iPhone Airton";
+char senhaWifi[] = "2153818aa";
+char serverAddress[] = "https://api.tago.io/data";
+char contentHeader[] = "application/json";
+char tokenHeader[] = "9f26994a-ceaa-4a71-94fb-26ec628ce0fe";
 
-int ledVermelho = 11;
-int ledAmarelo = 12;
-int ledVerde = 13;
-int buzzer = 10;
-int sensorLDR = A5;
-int sensorTMP = A4;
+int DHTPin = 4;
+int ldrPin = 2;
 
-int limiteLDR = 600;
+DHT dht(DHTPin, DHTTYPE);
 
-double tensao = 0; //declarando as variaveis
-double temp = 0;
+float temperatura;
+float umidade;
+float luminosidade;
 
-void setup()
-{
-  pinMode(ledVermelho, OUTPUT);
-  pinMode(ledAmarelo, OUTPUT);
-  pinMode(ledVerde, OUTPUT);
-  pinMode(buzzer, OUTPUT);
-  pinMode(sensorLDR, INPUT);
-  pinMode(sensorTMP, INPUT);
-  
+void setup() {
+  pinMode(DHTPin, INPUT);
+  pinMode(ldrPin, INPUT);
+  dht.begin();
   Serial.begin(9600);
-  lcd.begin(16,2); 
-   
+  init_wifi();
 }
 
-void loop(){
-  
-  int sensorUmidade = analogRead(A3);
-  int umidade = (map(sensorUmidade, 0, 1023, 10, 70));
-  int leituraL = analogRead(sensorLDR);
-  int leituraT = analogRead(sensorTMP);
-  tensao = (leituraT / 1023.0) * 5000; // calculo pra simular o sensor dht
-  temp = (tensao - 500) * 0.1;
-  
-  Serial.print("\t Iluminação: ");
-  Serial.print(leituraL, 1);
-  
-  Serial.print("\t Umidade: "); //valores de umidade no monitor
-  Serial.print(map(sensorUmidade, 0, 1023, 10, 70));
-  Serial.println("%");
-  
-  Serial.print("\t Temperatura = "); //valores de temperatura no monitor
-  Serial.print(temp,1);
+void loop() {
+  temperatura = dht.readTemperature();
+  umidade = dht.readHumidity();
+  luminosidade = analogRead(ldrPin);
 
- if ((leituraL >= 90) && (leituraL <= 70)) {
-    lcd.clear();
-    digitalWrite(ledAmarelo, HIGH);
-    digitalWrite(ledVermelho, LOW);
-    digitalWrite(ledVerde, LOW);
-    digitalWrite(buzzer, LOW);
-    lcd.setCursor(0,0);
-    lcd.print("Ambiente a meia");
-    lcd.setCursor(0,1);
-    lcd.print("luz");
-    delay(5000);
-      
+  send_temperatura();
+  send_umidade();
+  send_luminosidade();
+  delay(5000);
+}
+
+void init_wifi() {
+  Serial.println("Conectando WiFi");
+  Serial.print("Wifi: ");
+  Serial.println(nomeWifi);
+  WiFi.begin(nomeWifi, senhaWifi);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
-  else {
-    lcd.clear();
-    digitalWrite(ledAmarelo, LOW);
-    digitalWrite(ledVerde, LOW);
-    digitalWrite(ledVermelho, HIGH);
-    digitalWrite(buzzer, HIGH); // liga o buzzer
-    lcd.setCursor(0,0);
-    lcd.print("Iluminação");
-    lcd.setCursor(0,1);
-    lcd.print("Irregular ");
-    delay(5000);
-  }    
-  if ((temp >= 8) && (temp <= 13)){
-      lcd.clear();
-      lcd.setCursor(0,0);
-      lcd.print("Temperatura OK");
-      lcd.setCursor(0,1);
-	  lcd.print("Temp. = ");
-	  lcd.print(temp, 1);
-      lcd.print("C");
-      delay(5000);
-  }
-  else if (temp > 13){
-    lcd.clear();
-    digitalWrite(ledAmarelo, HIGH);
-    digitalWrite(buzzer, HIGH);
-    lcd.setCursor(0,0);
-    lcd.print("Temp. ALTA ");
-    lcd.setCursor(0,1);
-    lcd.print("Temp. = ");
-    lcd.print(temp, 1);
-    lcd.print("C");
-    delay(5000);
-  }
-  else{
-    lcd.clear();
-    digitalWrite(ledAmarelo, HIGH);
-    digitalWrite(buzzer, HIGH);
-    lcd.setCursor(0,0);
-    lcd.print("Temp. BAIXA ");
-    lcd.setCursor(0,1);
-    lcd.print("Temp. = ");
-    lcd.print(temp, 1);
-    lcd.print("C");
-    delay(5000);
-  }
-  
-  if ((umidade >= 60) && (umidade <= 80)){
-    digitalWrite(buzzer, LOW);
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Umidade OK ");
-    lcd.setCursor(0,1);
-	lcd.print("Umidade = ");
-	lcd.print(umidade, 1);
-    lcd.print("%");
-    delay(5000);
-  }
-  else if (umidade <= 59){
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Umidade BAIXA ");
-    lcd.setCursor(0,1);
-	lcd.print("Umidade = ");
-	lcd.print(umidade, 1);
-    lcd.print("%");
-    delay(5000);
-  }
-  else{
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Umidade ALTA ");
-    lcd.setCursor(0,1);
-	lcd.print("Umidade = ");
-	lcd.print(umidade, 1);
-    lcd.print("%");
-    delay(5000);
-  } 
+  Serial.println("\nWiFi Conectado");
+  Serial.print("Meu IP eh: ");
+  Serial.println(WiFi.localIP());
+}
+
+void send_temperatura() {
+  char postData[300];
+  int statusCode = 0;
+  strcpy(postData, "{\n\t\"variable\": \"temperatura\",\n\t\"value\": ");
+  char anyData[30];
+  dtostrf(temperatura, 6, 2, anyData);
+  strncat(postData, anyData, 100);
+  strcat(postData, ",\n\t\"unit\": \"C\"\n\t}\n");
+  Serial.println(postData);
+  client.begin(serverAddress);
+  client.addHeader("Content-Type", contentHeader);
+  client.addHeader("Device-Token", tokenHeader);
+  statusCode = client.POST(postData);
+  delay(2000);
+  // read the status code and body of the response
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+  Serial.println("End of POST to TagoIO");
+  Serial.println();
+}
+
+void send_umidade() {
+  char postData[300];
+  int statusCode = 0;
+  strcpy(postData, "{\n\t\"variable\": \"umidade\",\n\t\"value\": ");
+  char anyData[30];
+  dtostrf(umidade, 6, 2, anyData);
+  strncat(postData, anyData, 100);
+  strcat(postData, ",\n\t\"unit\": \"%\"\n\t}\n");
+  Serial.println(postData);
+  client.begin(serverAddress);
+  client.addHeader("Content-Type", contentHeader);
+  client.addHeader("Device-Token", tokenHeader);
+  statusCode = client.POST(postData);
+  delay(2000);
+  // read the status code and body of the response
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+  Serial.println("End of POST to TagoIO");
+  Serial.println();
+}
+
+void send_luminosidade() {
+  char postData[300];
+  int statusCode = 0;
+  strcpy(postData, "{\n\t\"variable\": \"luminosidade\",\n\t\"value\": ");
+  char anyData[30];
+  dtostrf(luminosidade, 6, 2, anyData);
+  strncat(postData, anyData, 100);
+  strcat(postData, ",\n\t\"unit\": \"Lúmen\"\n\t}\n");
+  Serial.println(postData);
+  client.begin(serverAddress);
+  client.addHeader("Content-Type", contentHeader);
+  client.addHeader("Device-Token", tokenHeader);
+  statusCode = client.POST(postData);
+  delay(2000);
+  // read the status code and body of the response
+  Serial.print("Status code: ");
+  Serial.println(statusCode);
+  Serial.println("End of POST to TagoIO");
+  Serial.println();
 }
